06b1b1f245428fbc88e40c9a05eab26e58e340b1
numan0010 <numan3102000@gmail.com>
2022-01-27 09:16:23 +0530

    landing-page-app

---
+++ js/script.js
@@ -0,0 +1,19 @@
+window.addEventListener('load', () => {
+    let toggleBtn = document.querySelector('.toggleBar');
+    let navBar = document.querySelector('.navBar');
+    let active = false;
+    toggleBtn.addEventListener('click', () => {
+        if (active == false) {
+            toggleBtn.classList.add('open');
+            navBar.style.display = "block"
+            active = true;
+
+        } else {
+            toggleBtn.classList.remove('open');
+            navBar.style.display = "none"
+            active = false;
+        }
+
+    })
+
+})
