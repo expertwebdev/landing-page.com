0bdf12873d9ec8823c62669edf3332a3ff863aa2
expertwebdev <numan3102000@gmail.com>
2022-01-31 12:46:41 +0530

    manifest

---
+++ manifest.js
@@ -0,0 +1,27 @@
+{
+    "name": "Alpha II",
+    "short_name": "AlphaII",
+    "start_url": "#",
+    "display": "standalone",
+    "background_color": "#fff",
+    "theme_color" : "#031e3e",
+    "description": "Alpha Plan II",
+    "icons": [
+      {
+        "src": "img/icon-96.png",
+        "sizes": "96x96",
+        "type": "image/png"
+      },
+      {
+        "src": "img/icon-192.png",
+        "sizes": "192x192",
+        "type": "image/png"
+      },
+      {
+        "src": "img/icon-512.png",
+        "sizes": "512x512",
+        "type": "image/png"
+      }
+    ]
+  }
+  
