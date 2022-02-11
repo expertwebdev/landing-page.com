5848ce7d3e1a131ffa773cfad08772f38d8fc207
expertwebdev <numan3102000@gmail.com>
2022-02-11 21:17:55 +0530

    slider added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', () => {
+
     let toggleBtn = document.querySelector('.toggleBar');
     let navBar = document.querySelector('.navBar');
     let active = false;
@@ -16,4 +16,43 @@ window.addEventListener('load', () => {
 
     })
 
-})
+   // slideshow
+   var slideIndex = 1;
+   showSlides(slideIndex);
+   
+   // Next/previous controls
+   function plusSlides(n) {
+     showSlides(slideIndex += n);
+   }
+   
+   // Thumbnail image controls
+   function currentSlide(n) {
+     showSlides(slideIndex = n);
+   }
+   
+   function showSlides(n) {
+     var i;
+     var slides = document.getElementsByClassName("mySlides");
+     if (n > slides.length) {slideIndex = 1}
+     if (n < 1) {slideIndex = slides.length}
+     for (i = 0; i < slides.length; i++) {
+         slides[i].style.display = "none";
+     }
+     slides[slideIndex-1].style.display = "block";
+   }
+   
+
+   // auto slider
+   showSlides(slideIndex);
+   
+   function showSlides() {
+     var i;
+     var slides = document.getElementsByClassName("mySlides");
+     for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";
+     }
+     slideIndex++;
+     if (slideIndex > slides.length) {slideIndex = 1}
+     slides[slideIndex-1].style.display = "block";
+     setTimeout(showSlides, 2000); // Change image every 2 seconds
+   }
